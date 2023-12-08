f7df0a04718942fcd70b1bfb756e2a55fa8057f5
Hadil24A <hadil.ahmed@hva.nl>
2023-12-08 10:14:24 +0100

    comment section

---
+++ main.js
@@ -0,0 +1,9 @@
+const comment = document.querySelector(".msg-show")
+const button = document.querySelector(".place-comment")
+
+button.addEventlistener('click', function () {
+    msg.classlist.toggle("msg-show");
+});
+
+onclick = document.getElementById("button");
+msg.classlist.toggle("msg-show");
