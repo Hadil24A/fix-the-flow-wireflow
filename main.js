e4a76b7eb23e14558b71d7054cc2b330b50a789b
Hadil24A <hadil.ahmed@hva.nl>
2023-12-09 20:56:29 +0100

    javascript proberen

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const comment = document.querySelector(".msg-show")
-const button = document.querySelector(".place-comment")
+// const comment = document.querySelector(".msg-show")
+// const button = document.querySelector(".place-comment")
 
-button.addEventlistener('click', function () {
-    msg.classlist.toggle("msg-show");
-});
+// button.addEventlistener('click', function () {
+//     msg.classlist.toggle("msg-show");
+// });
 
-onclick = document.getElementById("button");
-msg.classlist.toggle("msg-show");
+// onclick = document.getElementById("button");
+// msg.classlist.toggle("msg-show");
+
+let add = () => {
+    let text = document.querySelector('.newmsg #text');
+
+    if (text.value !== "" && comment.value != "") {
+        let list_item = document.createElement ("l1");
+        p.innerHTML = 
+        list_item.innerHTML = '
+            <span>
+            <p>${text.value} ${now}</p> 
+            </span>
+            <p>${comment.value}</p>
+            ';
+            list.append(list_item); 
+            }
+}
+
+
+
+
