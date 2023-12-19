c782995128528b3c48fb020088177e5967f1abcf
hadil244 <hadil.ahmed@hva.nl>
2023-12-19 15:24:39 +0100

    javascript veranderingen

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-let button = document.getElementsByClassName('place-comment')
+// Elementen selecteren; button, opmerkingensectie, bericht
+
+let button = document.querySelector('.place-comment')
+let commentBox = document.querySelector('.comment-section')
+let message = document.querySelector('.msg')
+let input = document.querySelector('.newmsg')
+
+// Luisteren naar event; button 
 
 button.addEventListener('click', function() {
 
-    button.classList.add('show-msg')
+// Text-area krijgt een border en bericht verschijnt
+
+    input.classList.toggle('.inputfocus')
+    message.classList.add('.show-msg')
+    
 })
+
+
+
+
+
+
+
+
+
